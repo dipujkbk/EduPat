22c9d093a81ea6c5abc5ead7419ef666aa9b05c2
Priyabrat Routray <dipujkbk@gmail.com>
2023-11-03 11:37:39 +0530

    course slice and corresponding reducer added

---
+++ src/components/core/Dashboard/AddCourse/RenderSteps.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function RenderSteps() {
+
+    const steps = [
+        {
+            id: 1,
+            title: "Course Information",
+        },
+
+        {
+            id: 2,
+            title: "Course Builder",
+        },
+        {
+            id: 3,
+            title: "Publish",
+        },
+    ]
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RenderSteps
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -3,12 +3,15 @@ import authReducer from "../slices/authSlice";
 import profileReducer from "../slices/profileSlice";
 import cartReducer from "../slices/cartSlice"
 import viewCourseReducer from "../slices/viewCourseSlice"
+import courseReducer from "../slices/courseSlice"
 
 const rootReducer = combineReducers({
     auth: authReducer,
     profile: profileReducer,
     cart: cartReducer,
     viewCourse: viewCourseReducer,
+    course: courseReducer
+
 })
 
 export default rootReducer
